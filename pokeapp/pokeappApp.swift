4bb0f008b4a231d2c7a15a3965acdd8b99e665f1
Eibiel Sardjanto <eibiel260898@gmail.com>
2023-12-07 06:44:57 +0700

    Initial Commit

---
+++ pokeapp/pokeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeappApp.swift
+//  pokeapp
+//
+//  Created by MAC-097419 on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pokeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
